1f7212372846708b12da2ffbc7df7a3f6664448b
Vinicius Ribeiro <viniciusribeirosp@gmail.com>
2017-01-06 19:00:56 -0200

    Add comandos aprendidos

---
+++ comandos_aprendidos.sh
@@ -0,0 +1,16 @@
+##comandos aprendidos
+
+
+#configurações iniciais 
+#setando sublime como editor principal do git
+git config --global core.editor "subl -n -w"
+git config --global push.default upstream
+git config --global merge.conflictstyle diff3
+
+#setando meu nome e email
+git config --global user.email "viniciusribeirosp@gmail.com"
+git config --global user.name "Vinicius Ribeiro"
+
+
+#Mostra lista de configuração
+git config --list
